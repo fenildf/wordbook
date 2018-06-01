f79b34e31dde4d87f4f417a3afde95ad02217188
Spencer <pallyoung@163.com>
2018-06-01 17:13:05 +0800

    native

--- wordbookapp/android/app/src/main/java/host/spencer/wordbookapp/MainApplication.java
+++ wordbookapp/android/app/src/main/java/host/spencer/wordbookapp/MainApplication.java
@@ -4,10 +4,13 @@ import android.app.Application;
 
 import host.spencer.rnpackage.RNPackage;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new RNPackage()
+              new MainReactPackage(),
+              new VectorIconsPackage(),
+              new RNPackage(),
+              new SQLitePluginPackage()
       );
     }
 
