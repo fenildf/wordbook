061beda12f0be180db0319a7c04e6762470f88a9
Spencer <pallyoung@163.com>
2018-06-01 10:59:47 +0800

    wordbook

--- scripts/wordbook.js
+++ scripts/wordbook.js
@@ -6,22 +6,63 @@ const path = require('path');
 const SEP = path.sep;
 
 const BOOK_PATH = 'wordbooks';
+const USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
+
+function bookPath(name){
+    return BOOK_PATH+SEP+name+'.json';
+}
 function removeIfExist(path){
     if(fs.existsSync(path)){
         fs.unlinkSync(path);
     }
 }
 
-function book(name,html){
+function Item(name,children){
+    let item = {
+        name
+    }
+    if(children){
+        item.children = children;
+    }
+    return item;
+}
+function seekItems(items,childrenHTML){
+    return items.map((item)=>{
+        let id = item.match(/#panel[\d]{1,}/)[0];
+        let name = item.match(/(?=>).*?(?=<)/)[0].replace('>','');
+        let children = childrenHTML[id.match(/[\d]{1,}/)[0]-1].match(/<a href="[\w\+]*?\.html" class="inlink">[\w\s]*?<\/a>/gi);
+        return Item(name,children.map((child,i)=>{
+            return Item(child.match(/>[\w\s]*?(?=<)/)[0].replace('>',''));
+        }));
+    });
 
 }
+function book(name,html){
+    try{
+        let items = html.match(/<a href="#panel[\d]{1,}" class="inlink">[\s\S]*?<\/a>/gi);
+        let childrenHTML = html.match(/<ul class="txtlist arrow">[\s\S]*?<\/ul>/gi).slice(1);
+        let children = seekItems(items,childrenHTML);
+        let book = {
+            name,
+            children
+        };
+        let bPath = bookPath(name);
+        fs.createWriteStream(bPath).end(JSON.stringify(book,null,'\t'));
 
-bookurls.forEach(function(item){
+    }catch(e){
+        console.log(e.message,name)
+    }
+}
+
+bookurls.forEach(function(item,i){
+    // if(i!==0){
+    //     return;
+    // }
     let {
         name,
         url
     } = item;
-    fetch(url).then(function(response){
+    fetch(url,{headers:{'user-agent':USER_AGENT}}).then(function(response){
         response.text().then(function(html){
             book(name,html);
         });
