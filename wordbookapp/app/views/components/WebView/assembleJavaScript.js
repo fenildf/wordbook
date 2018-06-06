96ecf87825e0bd2210d0ccf6b019dcaef9f97642
Spencer <pallyoung@163.com>
2018-06-06 11:14:16 +0800

    webview

--- wordbookapp/app/views/components/WebView/assembleJavaScript.js
+++ wordbookapp/app/views/components/WebView/assembleJavaScript.js
@@ -1,28 +1,75 @@
+import { WSAENAMETOOLONG } from "constants";
 
 
 
 
-function bridge(){
-    var id = 1;
+function bridge() {
     var callbacks = [];
 
-    window.invokeMethod = function(methodName,params,callback){
-        var message = {
-            id:id,
-            method:methodName,
-            params:params,
-        }
-        id++;
-        window.postMessage(JSON.stringify(message),'*');
+    var methods = {
+
     }
 
-    window.document.addEventListener('message',function(data){
+    var originalPostMessage = window.postMessage;
+    var patchedPostMessage = function (message, targetOrigin, transfer) {
+        originalPostMessage(message, targetOrigin, transfer);
+    };
+    patchedPostMessage.toString = function () {
+        return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+    };
+    window.postMessage = patchedPostMessage;
 
+    window.nativeWebView = {
+        invokeMethod: function (methodName, params, callback) {
+            var message = {
+                method: methodName,
+                params: params||[],
+                type:'invokeMethod'
+            }
+
+            if (callback) {
+                callbacks.push(callback);
+                message.callback = callbacks.length - 1;
+            }
+            window.postMessage(JSON.stringify(message),'*');
+        },
+        injectMethod:function(methodName,method){
+            methods[methodName] = method;
+        }
+    }
+    window.nativeWebView.invokeMethod.toString = function(){
+        return 'function invokeMethod() { [native code] }'
+    }
+    window.nativeWebView.injectMethod.toString = function(){
+        return 'function injectMethod() { [native code] }'
+    }
+    window.document.addEventListener('message', function (data) {
+        try{
+            data = JSON.parse(data);
+            switch (data.type){
+                case 'invokeMethod':
+                    if(data.method){
+                        methods[data.method].apply(null,data.params);
+                    }
+                case 'invokeMethodCallback':
+                    if(data.callback){
+                        callbacks[data.callback].apply(null,data.params);
+                        delete  callbacks[data.callback];
+                    }
+            }
+            
+        }catch(e){
+
+        }
     });
+    setTimeout( ()=>nativeWebView.invokeMethod('hello'),4000)
 }
 
-function assembleJavaScript(userJavaScript){
-
+function assembleJavaScript(userJavaScript) {
+    return `
+    (${bridge.toString()})();
+    ${userJavaScript||''}
+    `
 }
 
 export default assembleJavaScript;
--- wordbookapp/app/views/components/WebView/index.js
+++ wordbookapp/app/views/components/WebView/index.js
@@ -1,23 +1,83 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
-import {WebView as Web} from 'react-native';
+import { WebView as Web } from 'react-native';
 
+import assemebleJavaScript from './assembleJavaScript';
 
+const WEB_REF = 'WEB_REF';
 
-class WebView extends Component{
-    constructor(...props){
+class WebView extends Component {
+    constructor(...props) {
         super(...props);
+        this.state = {
+
+        }
+        this._callbacks = [];
+        this._methods = {
+
+        }
     }
-    execJavascript(js){
+    componentWillUnmount() {
+        this._callbacks = [];
+        this._methods = null;
+    }
+    execJavascript(js) {
 
     }
-    render(){
+    invokeMethod(method, params, callback) {
+        let data = {method,params, type: 'invokeMethod'};
+        if(callback){
+            this._callbacks.push(callback);
+            data.callback = this._callbacks.length-1;
+        }
+        this._postData(data);
+    }
+    injectMethod(methodName, method) {
+        this._methods[methodName] = method;
+    }
+    _postData(data) {
+        this.refs[WEB_REF].postMessage(JSON.stringify(data));
+    }
+    _onMessage = ({ nativeEvent }) => {
+        try {
+            data = JSON.parse(data);
+            switch (data.type) {
+                case 'invokeMethod':
+                    let method = this._methods[data.method];
+                    if (method) {
+                        Promise.resolve(method.apply(null, data.params)).then((result)=>{
+                            this._postData({
+                                callback
+                            })
+                        });
+                    }
+                case 'invokeMethodCallback':
+                    let callback = this._callbacks[data.callback];
+                    if (callback) {
+                        callback.call(null, data.params);
+                        delete this._callbacks[data.callback];
+                    }
+            }
+
+        } catch (e) {
+
+        }
+
+
+
+    }
+    render() {
         let props = this.props;
         let injectedJavaScript = props.injectedJavaScript;
         return (
-            <Web 
+            <Web
+                ref={WEB_REF}
+                bounces={false}
                 {...props}
-                />
+                source={{ html: '<div>123456</div>' }}
+                onMessage={this._onMessage}
+                injectedJavaScript={assemebleJavaScript(injectedJavaScript)}
+            />
         )
     }
 }
