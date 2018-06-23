52ba1b25c702d60a38b66edef74711fb33650b42
Spencer <pallyoung@163.com>
2018-06-23 16:15:12 +0800

    book

---
+++ scripts/waiyanse.js
@@ -0,0 +1,1356 @@
+const source = `一年级上
+Module 1
+hello 你好
+am (I'm = I am) 是（我是）
+goodbye (bye) 再见
+How are you? 你好么？
+no 不，不是
+yes 是，是的
+sorry 对不起
+fine （身体）很好
+thank 谢谢
+you 你
+Module 2
+what 什么
+is (what's = what is) 是（是什么）
+your 你的
+name 名字
+please 请
+good 好的
+morning 早晨，上午
+Ms 女士
+my 我的
+bird 鸟 
+boy 男孩
+girl 女孩
+afternoon 下午
+panda 熊猫 
+Module 3
+sit 坐
+down 向下
+stand 站
+up 向上
+open 打开
+the 这（那）个；这（那）些
+window 窗户
+door 门
+See you! 再见
+point 指
+to 向……
+desk 书桌
+chair 椅子
+Module 4 
+it 它
+it's = it is 它是
+red 红色；红色的
+blue 蓝色；蓝色的
+yellow 黄色；黄色的
+colour 颜色
+green 绿色；绿色的
+now 现在
+black 黑色；黑色的
+white 白色；白色的
+dog 狗
+cat 猫
+and 和
+look 看
+orange 橙色；橙色的 
+Module 5 
+this 这个 
+our 我们的 
+teacher 教师
+school 学校
+classroom 教室
+Nice to meet you见到你真高兴
+child 小孩，儿童
+that 那个
+bag 包
+Module 6 
+schoolbag 书包
+book 书
+pen 钢笔
+pencil 铅笔
+ruler 直尺
+eraser 橡皮
+pencil case 铅笔盒
+crayon 蜡笔 
+Module 7 
+new 新的
+kite 风筝
+or 或者
+not (isn't = is not) 不（不是）
+monkey 猴子
+monster 怪物
+help 救命（呼救用语）
+house 房屋，住宅
+box 箱，盒 
+Module 8 
+how many 多少
+one 一
+two 二
+three 三
+four 四
+count 数
+say 说
+five 五
+six 六
+seven 七
+eight 八
+nine 九
+ten 十
+pink 粉色；粉色的
+ball 球
+so 这么，那么
+many 许多
+brown 棕色；棕色的
+Module 9
+how old 多大
+are 是
+too 也
+happy 快乐的
+birthday 生日
+but 但是，可是
+for 为，给，对
+You're welcome! 不客气！
+Module 10 
+father 父亲
+daddy 爸爸
+mother 母亲
+grandpa 祖父；外祖父
+grandma 祖母；外祖母
+friend 朋友
+his 他的
+car 汽车，轿车
+sister 姐妹
+brother 兄弟
+her 她的
+一年级下
+Module 1 
+he(heis=he’s)他（他是）
+doctor医生
+she(she is=she’s)她她是
+me我（宾格）
+nurse护士
+driver司机
+policeman警察
+pupil学生小学生
+Module 2 
+Where/where's=where is哪里/在哪里
+in在……里
+on在……上
+under在…… 下
+toy玩具
+bed床
+look at看
+bear熊
+Module 3 
+eleven十一
+twelve十二
+don't=do not不
+know知道
+other其他的
+fish鱼
+tree树
+let's=let us让我们
+Module 4 
+head头
+touch触摸
+nose鼻子
+face脸
+ear耳朵
+mouth嘴
+eye眼睛
+body身体
+these这些
+leg腿
+hand手
+foot feet(复数)脚 足
+baby婴儿
+Module 5 
+they他（她/它）们
+they're=they are他（她/它）们是
+cow奶牛
+pig猪
+farm农场
+duck鸭子
+chichen鸡
+egg鸡蛋
+thin瘦的
+very很，非常
+fat胖的
+big大的
+little小的
+Module 6
+snake蛇
+short矮的 短的
+small小的
+elephant大象
+go去
+zoo动物园
+long长的
+giraffe长颈鹿
+tall高的
+tiger老虎
+cute可爱的 乖巧的
+strong强壮的
+horse马
+aren't=are not不 不是
+Module 7 
+there is有，存在
+animal动物
+can可以,能
+see看见
+there are有，存在(复数)
+help帮助
+over there在那边
+table桌子
+we我们
+some一些
+Module 8 
+a pair of一双… 一对… 
+shorts（常复）短裤
+shirt衬衫
+shoe鞋子
+swim游泳
+sock短袜
+put on穿上，戴上
+clothers(常复)衣服
+here这里
+football足球（运动）
+basketball篮球（运动）
+ping-pong乒乓球（运动）
+Module 9 
+like喜欢
+lion狮子
+them他（她/它）们(宾格）
+all全部，所有
+play参加（运动、比赛）玩耍
+with用…… 
+together一起
+favourite最喜欢的，最喜爱的
+swimming游泳（运动）
+Module 10 
+idea主意，想法
+hot热的
+go swimming去游泳
+sing唱，唱歌
+dance跳舞
+tired累的，疲惫的
+二年级上
+Module 1 
+song 歌曲
+come on 加油；快点
+team 球队，运动队
+Module 2 
+meat 肉
+noodles （常复）面条
+rice 米饭
+sweets （常复）糖果
+children（child复数）孩子们
+ginger 姜
+nice 好的
+onion 洋葱
+ice cream 冰淇淋
+Module 3 
+banana 香蕉
+apple 苹果
+milk 奶，牛奶
+orange 柑橘，橙
+fruit 水果
+milkshake 奶昔
+Here you are. 给你。
+everything 每件事，所有事物
+tomato 番茄，西红柿
+really 很，非常
+their 他（她/它）们的
+favorite 最喜欢的事物
+Module 4 
+T-shirt T恤衫
+here's = here is 这是
+dress 连衣裙
+too 太……
+one 一个
+doesn't = does not 不
+trousers 裤子
+party 聚会
+Module 5 
+get up 起床 
+poster 海报，招贴画 
+go to school 去上学 
+have 吃，喝 
+lunch 午餐 
+have lunch 吃午餐 
+home 家 
+go home 回家 
+watch 观看 
+watch TV 看电视
+Module 6 
+on 在……的时候 
+Sunday 星期日 
+park 公园 
+great 太好了，好极了 
+goal 进球，得分 
+live 居住 
+moon 月亮 
+sky 天，天空 
+city 城市
+Module 7 
+how 怎样，如何
+train 火车
+bus 公共汽车
+beep （汽车喇叭）嘟嘟声
+by 乘，坐（交通工具）
+walk　步行
+How about...? ……怎么样？
+goes（go第三人称单数）去
+work 工作地点；工作
+go to work 去上班
+bike 自行车
+with 与…在一起；同；跟；和
+run 跑，奔跑
+holiday 假期
+on holiday 度假
+then 然后
+ship 轮船
+plane 飞机
+love 喜爱
+Module 8 
+watches（watch第三人称单数形式）观看
+weekend 周末
+read 阅读
+come 来
+play 演奏（乐器）
+piano 钢琴
+music 音乐
+listen 听
+listen to... 听……
+drum 鼓
+Module 9 
+winter 冬天
+fly 放飞（风筝）
+in 在……期间，在……时候
+spring 春天
+summer 夏天 
+autumn 秋天
+skating 滑冰
+go skating 去滑冰
+warm 温暖的
+wear 穿、戴
+jacket 夹克、短上衣
+sunglasses 太阳镜
+cool 凉爽的；酷的
+sweater 毛线衫
+glove 手套
+cold 寒冷的
+coat 外套
+Module 10 
+year 年，年份
+Happy New Year! 新年快乐！
+come in 进来
+present 礼物
+dumpling 水饺
+eat 吃
+Chinese 中国的
+hair 头发
+firecracker 鞭炮，爆竹
+have 有
+Christmas 圣诞节
+wish 祝，祝愿
+dinner 正餐，主餐
+二年级下
+Module 1 
+weather天气
+like像，如同
+What's the weather like?天气怎么样？
+sunny阳光明媚的
+windy 有风的，刮风的
+take 带，拿
+cap（有帽檐的）帽子
+sofa 沙发
+street街道
+rain 下雨；雨
+snow雪
+ski滑雪
+Module 2 
+radio收音机；电台节目
+photo照片
+newspaper报纸
+little年幼的，幼小的
+camera照相机
+smile微笑
+draw绘画，画
+picture 图画；照片
+write 写
+letter信
+colour给……着色
+Module 3 
+tidy整理；收拾
+room房间
+do做，干
+homework家庭作业
+sleep睡觉
+secret 秘密
+card贺卡
+flower花
+science科学
+today今天
+Module 4 
+call打电话，通话
+noise 声音，响声
+drink 喝，饮
+talk 说话，交谈
+stone石子，石头
+five-stones 五石游戏
+behind在……后面
+hide躲藏
+seek寻找
+hide-and-seek捉迷藏
+Module 5 
+skip跳绳
+hurry up 赶快
+late 晚的，迟的
+sad伤心的，难过的
+no 没有
+those 那些
+clap 拍手
+game 游戏
+clapping game拍手游戏
+well噢，喔，好
+kid小孩
+catch抓，捉；捉人游戏
+of course当然
+Module 6
+usually通常地
+us（宾格）我们
+ride骑
+thing事情，活动
+ill有病的
+cook做（饭），烧（菜）
+her（宾格）她
+goshopping去购物
+kitchen厨房 
+Module 7
+Children's　Day儿童节
+TV电视台
+play（戏剧）演出
+say念，朗诵
+poem诗
+dragon龙
+dragon　dance舞龙
+picnic野餐
+family家，家庭
+beach海滩 
+Module 8 
+hill小山
+past经过
+station车站
+hospital医院
+stop停止
+turn转向，转弯
+around到另一面，转向
+turn　around转身，回转
+ground地面
+again又一次，再一次
+jump跳跃
+back回到，返回
+come back回来 
+Module 9 
+left向左；左边的
+turnleft向左转
+right向右；右边的
+turnright向右转
+road公路，道路
+excuseme劳驾，对不起
+straight直地，直线地
+go　straight　on直着走
+lost迷路的
+out出，向外
+factory工厂 
+Module 10 
+next　to接近，靠近
+cousin堂（表）兄（弟姐妹）
+supermarket超市
+wrong不正确的
+way道路，路线
+turn　back往回走
+front前面
+infront　of在……前面
+well完全地，充分地
+taxi出租车，计程车
+Mrs太太，夫人
+Cinema电影院
+三年级上
+Module 1 
+use使用
+chopsticks（常复）筷子
+want想要
+hungry饥饿的
+mess肮脏，凌乱
+hard困难的
+knife刀
+fork叉
+a knife and fork一副刀叉
+easy容易的
+hamburger汉堡包
+chips（常复）炸薯条
+grass草
+food食物
+fast　food快餐
+Module 2 
+make制作
+cake蛋糕
+mum妈妈
+dad爸爸
+quiet安静的，不出声的
+water给（植物）浇水；水
+lovely可爱的
+Module 3 
+naughty淘气的
+paper纸做的，纸质的
+boat小船，小舟
+dragon boat龙舟
+people人，人们
+row划（船）
+lake湖
+feed喂，为……提供食物
+bread面包
+get out走开，离开
+Module 4 
+far远
+fast快，快速地
+winner胜利者
+can't = cannot不能
+slow慢的
+fly飞
+koala树袋熊，考拉
+climb爬
+cry哭
+Module 5 
+drink一杯（份）饮料
+shop商店
+go out外出
+thanks 谢谢
+library图书馆，图书室
+worry担心
+Module 6 
+have got有
+careful小心的
+wash洗
+has got（have got的第三人称单数形式）有
+pear梨
+peach桃子
+Module 7 
+headache头痛
+stomachache胃痛；腹痛
+test考试
+Friday星期五
+clever聪明的
+cold感冒
+class课
+begin开始
+cough咳嗽
+Module 8 
+bring带来
+umbrella雨伞
+get 拿，取
+which哪一个
+broken损坏的
+Module 9 
+long jump跳远
+make friends交朋友
+sports day运动日
+high高的
+high jump跳高
+race赛跑
+run a race（参加）赛跑
+star明星；星
+uncle伯父；叔父；姑父；姨父
+Module 10 
+airport飞机场
+sea大海
+visit拜访（某人），参观（某地）
+Saturday星期六
+film电影
+三年级下
+Module 1 
+nice友好的，亲切的
+always总是，一直
+a bit稍微，有点儿
+shy害羞的，胆怯的
+e-card电子贺卡
+quiet文静的
+another另一个，再一个
+parrot鹦鹉
+twenty二十
+helpful肯帮忙的，有用的
+maths数学，数学课
+plus加，加上
+more更多的，另外的 
+Module 2 
+about 关于
+river河
+wide宽的，宽阔的
+old年代久的，古老的
+clock钟，时钟
+wheel轮子
+round 圆形的，球形的
+will将，将会
+you’ll=you will你将
+tower塔
+bridge桥
+Module 3 
+won’t=will not 将不
+countryside农村，乡下
+farmer农民，农场主
+there在那里
+lots of许多的，大量的
+breakfast早餐
+have breakfast吃早餐
+tea茶，茶叶
+tea party（下午的）茶会，茶话会
+Module 4 
+pick采，摘（花，水果）
+fourteen十四
+sixteen十六
+eighteen十八
+thirteen十三
+fifteen十五
+seventeen十七
+nineteen十九
+afraid担心的
+feel感觉，觉得
+all right 好，行 
+Module 5 
+tomorrow明天
+Monday星期一
+why为什么
+why not？为什么不呢？
+Tuesday星期二
+Wednesday星期三
+Thursday星期四
+next下一个的，其次的
+week周，星期
+Module 6 
+stamp跺脚，用力踩
+computer计算机，电脑
+shall该，要
+arm胳膊 
+Module 7 
+class班级
+any一些，若干，任何一个
+ask问，询问，打听
+question问题
+forty四十
+so因此
+about大约
+thirty三十 
+Module 8 
+hard努力地
+lazy懒惰的，懒散的
+parent母亲，父亲
+Parents’ Day家长日
+speak谈话，说
+with对于
+quite很，非常
+good at擅长
+English英语，英语课
+PE=physical education体育，体育课
+Chinese汉语，中文，语文课
+art美术，艺术，美术课
+report成绩报告单
+try尝试，试图
+Module 9 
+were（are的过去式）是
+young年轻的
+beautiful漂亮的，美丽的
+was（is的过去式）是
+also也，还
+then那时
+who谁
+grandparent（外）祖父，（外）祖母
+old老的，年老的
+yesterday昨天
+weren’t=were not不是
+wasn’t=was not不是
+fun有趣的事
+Module 10
+second第二
+floor层
+worried担忧的，，焦虑的
+find发现，找到
+first第一
+remember记起，回想起
+Mother’s 　Day母亲节
+vegetable蔬菜
+last上一个
+四年级上
+Module 1 
+dear亲爱的
+well健康的
+of……的
+soon不久，很快
+tell告诉，讲述
+love爱你的
+birthday生日
+Module 2 
+skirt短裙
+clean打扫，清理
+finish完成
+dirty脏的
+paint绘画
+Mr先生
+phone打电话
+Module 3 
+didn't=did not不
+National Day国庆节
+stay停留
+what多么
+boring无聊的；令人厌烦的
+day一天；白天
+Module 4 
+invent发明，创造
+paper纸
+important重要的
+printing印刷术
+print印刷
+right正确的，对的
+between在……之间
+bicycle自行车
+mouse老鼠
+beside在……旁边
+man男人
+Module 5 
+went（go的过去式）去
+saw（see的过去式）看见
+trip出行，旅行
+took（take的过去式）需要花费
+fifty五十
+minute分钟
+top顶部
+mountain山
+ate（eat的过去式）吃
+candies（candy的复数形式）糖果
+plant植物
+had（have的过去式）度过
+have a good time过的愉快
+bought（buy的过去式）买
+wore（wear的过去式）穿，戴
+go on a trip去旅游
+ice冰
+at first起初，开始时
+fell（fall的过去式）掉下；摔倒
+fall over摔倒，跌倒
+hurt使疼痛；使受伤
+a lot在很大程度上
+able聪明能干的
+be able to能够
+skate滑冰
+Module 6 
+become变成
+gold金子
+ago以前，以往
+long ago很久以前
+magic有魔力的
+paintbrush画笔
+women妇女，女性
+became（become过去式）变成
+real真实的
+bad坏的，不好的
+took（take过去式）拿走
+away离去，离开
+leaves（leaf的复数形式）树叶
+only只，仅仅
+painting（一幅）画，绘画作品
+angry生气的，愤怒的
+said（say过去式）说
+Module 7 
+doll玩偶，玩具娃娃
+matter事情
+lost（lose的过去式）丢失
+found（find的过去式）发现
+key钥匙
+look for寻找
+favourite喜爱，爱好
+stand up站起来
+Module 8 
+when什么时候
+came（come的过去式）来
+competition比赛
+won（win的过去式）赢，获胜
+gold金制的
+cup奖杯
+flute长笛
+practise练习
+took（take的过去式）拍摄
+look看上去……
+a lot经常，长时间地
+Module 9 
+happen发生
+fall off从……跌落
+bike ride骑自行车
+thirsty渴的
+water水
+watermelon西瓜
+carried（carry的过去式）拿，搬
+bump碰，撞
+hurt（hurt的过去式）使疼痛；使受伤
+knee膝盖
+cut（cut的过去式）切，割
+finger手指
+Module 10 
+early早
+every每个，每一
+during在……期间
+computer game计算机游戏
+take服（药）
+medicine药
+a lot of许多的
+exercise运动，锻炼
+healthy有益健康的；健康的
+juice果汁
+before在……之前
+after在……之后
+toilet厕所
+四年级下
+Module 1 
+machine机器
+welcome欢迎
+crisp炸薯片
+sir先生
+potato马铃薯，土豆
+dangerous危险的
+put放，安放，放置
+wait等；等待
+each各自；每个
+Module 2 
+cheap便宜的
+expensive昂贵的
+send发送，寄
+email电子邮件
+cost花费，价钱为
+hundred一百
+Module 3 
+shout呼喊，大叫
+wolf狼
+sheep（sheep的复数形式）绵羊
+ran（run的过去式）跑
+once upon a time从前
+look after照顾，照料
+bored厌倦的；厌烦的
+village乡村
+everyone人人，每人
+laugh笑
+lie谎话，谎言
+learnt（learn的过去式）学习
+lesson教训；一节课
+wouldn't=would not将不
+into进入……里面
+month月份，月
+cheese奶酪
+scared惊恐的，害怕的
+run away逃走，逃跑
+go into a shop走进一家商店
+tell lies说谎
+learn a lesson学到一课（得到一次教训）
+Module 4 
+concert音乐会
+violin小提琴
+guitar吉他
+nervous紧张的
+end结束
+proud感到自豪的 ，感到骄傲的
+Module 5 
+surprise意想不到的礼物，意外之事，惊奇
+hear听见
+special特别的
+Father's Day父亲节
+message便条，信，口信
+clap鼓掌
+Module 6 
+great非常好的,令人愉快的
+report新闻报道
+stick粘贴
+fantastic极好的，极出色的
+fun有趣的
+meet与……见面
+east东，东方，东部
+west西，西方，西部
+o'clock……点钟
+at 10 o'clock 在10点钟
+in the west of在西部
+Module 7 
+time时间
+cartoon动画片
+much非常，很
+am上午
+pm下午
+lunch time 午饭时间
+Module 8 
+best最好的
+north北，北方，北部
+south 南， 南方，南部
+rest休息
+have a rest休息一下
+country国家
+speak说（某种语言）
+capital首都
+the capital of ……的首都
+maybe也许，大概
+map地图
+a map of China中国地图
+an old city一座古老的城市
+Module 9 
+because因为
+language语言
+Australia澳大利亚
+Australian澳大利亚的
+kangaroo袋鼠
+back后面的
+Module 10 
+postcard明信片
+plan计划
+museum博物馆
+restaurant餐馆，饭店
+every day每天
+五年级上
+Module1
+for达，计
+different不同的
+bench长凳
+then那么，就
+playground操场，运动场
+pen钢笔
+pencil铅笔,
+pencil case 文具盒
+pencil box铅笔盒
+ruler尺子
+eraser橡皮
+bag包
+schoolbag书包
+book书
+notebook笔记本
+Chinese book语文书
+English book英语书
+Maths book数学书
+postcard明信片
+crayon蜡笔
+Module2 
+heavy重的
+let允许，让
+sell卖，销售
+sometimes有时
+反义词
+good-bad 好-坏
+big-small/little大-小
+long/tall-short长-短
+young/new-old新-旧
+fat-thin 胖-瘦
+clean-dirty 干净-脏的
+happy-sad 开心-难过
+easy-hard 容易-困难
+cheap-expensive便宜-昂贵
+sick/ill-healthy 疾病-健康
+heavy-light 重-轻
+right-left（wrong） 右-左
+Module 3 
+Halloween万圣节前夕
+trick or treat不请吃就捣蛋
+give给，送
+festival节日
+scary吓人的，可怕的
+mask面具
+neighbour邻居
+night夜晚，夜间
+Easter复活节
+chick小鸡
+chocolate巧克力
+really 果真
+Module 4 
+stories（story的复数形式）故事
+invitation请帖
+tidy up收拾，整理
+November十一月
+from （表示时间）从……起
+to直到
+reply回答，答复
+street街道
+Module 5 
+mine我的
+whose谁的
+yours你的；你们的
+hers她的
+both两……（都），两个……（都）
+wet湿的
+That's OK.没关系。
+take away移走，拿走
+worker工人
+still还，仍然
+Module 6 
+well好；熟练地
+high高高地
+true真实的，确实的
+got（get的过去式）得到
+eighty八十
+point分数
+fan狂热爱好者，迷
+seventy七十
+sixty六十
+Module 7 
+useful有用的
+show（电视或广播）节目
+presenter（电视或广播节目的）主持人
+blind失明的，瞎的
+could（can的过去式）能
+speak说（某种语言）
+fire火灾，失火
+inside在……里面
+fire　fighter消防队员
+couldn't = could not不能
+Module 8 
+often经常
+around围绕，环绕
+start 开始
+line行，排，列
+half一半
+past晚于，过（几点）
+miss想念
+study学习
+open开着的
+wall墙
+have fun玩得开心
+break课间休息
+morning　exercise做早餐
+Module 9 
+nothing没有事情，没有东西
+think思考；想，觉得
+drank（drink的过去式）喝，饮
+Module l0 
+should应该
+go to bed上床睡觉
+shouldn't = should not不应该
+take down取下，拿下
+clean干净的
+rule规则，规章
+kind友好的，善意的，体贴的
+五年级下
+Module 1 
+drive驾驶
+drove(drive的过去式)驾驶
+player演奏者
+office办公室 
+fish捕鱼， 钓鱼
+Module 2 
+sausage香肠
+sandwich三明治
+fish　and　chips炸鱼加炸薯条
+traditional传统的
+dish食品，菜肴
+delicious美味的
+chicken鸡肉
+Module 3 
+DVD 数字化视频光盘
+Librarian 图书馆管理员，图书馆馆长
+Show 给（某人）看
+borrow 借入，借来
+Module 4 
+sent (send 的过去式) 寄，发送
+American 美国的，美国人的
+read (read 的过去式) 阅读	
+interesting 令人感兴趣的，有趣的
+season 季节
+snow 雪
+Module 5
+How much…?......多少钱？
+light轻的
+sales assistant售货员，营业员
+carry提，抱，扛，背，搬
+pound英镑
+back背，背部
+over在......的上方，在......的上面
+shoulder肩，肩膀
+Module 6
+build 建造
+place 地方
+housand （数字） 一千
+circle 圆，圆形
+answer 答案
+hope 希望
+so 如此，这样
+get 到达（某地）
+hour 小时
+amazing 令人吃惊的
+take 搭乘，乘坐
+short 短暂的，短期的
+helicopter 直升机
+Module7
+evening傍晚，晚上
+actor演员
+busy忙的，忙碌的
+quarter一刻钟
+Module8
+Chinese中国人
+drew（draw的过去式）绘画，画
+piece张，片，块
+painted（paint的过去式）给...涂颜料
+scissors（常复）剪刀
+stick棍，棒
+tied（tie的过去式）扎上，系上
+string线，细绳
+Module9
+theatre剧院
+men（man的复数形式）男人
+women（woman复数）女人
+told（tell过去式）口述，讲
+joke笑话
+funny滑稽的，可笑的
+show演出，表演
+put（put的过去式）放置
+history历史，历史学
+subject学科，科目
+Module10
+ticket票
+passport护照
+leave离开，动身
+July七月
+August八月
+met（meet的过去式）遇见
+classmate同班同学
+六年级上
+Module 1 
+how long多长
+near在……附近
+along沿着
+more than超过
+kilometre['kɪləumi:tə(r)] 千米，公里
+metre米
+office building办公大楼
+Module 2
+Chinatown唐人街，中国城
+town 城镇，市镇
+subject主题，话题
+everywhere到处
+spoke（speak的过去式）说（某种语言）
+lion dance舞狮
+tomb坟墓
+wrote（write的过去式）写
+when当……时
+Module 3 
+stamp[stæmp] 邮票
+book簿，册
+collect收集
+hobby爱好
+sun太阳
+island岛屿
+coconut['kəukənʌt]椰子
+Module 4
+Thanksgiving 感恩节
+nearly 差不多，奖金
+the Spring Festival 春节
+sure 当然
+December 十二月
+light 灯
+Module 5
+pleased [pli:zd] 开心的，高兴的
+pretty['priti]漂亮的
+French[frentʃ ] 法语
+phone电话
+Module 6 
+world世界
+difficult[ˈdɪfɪkəlt] 困难的
+answer回答，答复
+Miss（称呼）女士，小姐 
+Module 7 
+bamboo[ˌbæmˈbu ] 竹子
+gave（give的过去式）给，送
+its它的
+almost[ˈɔ:lməʊst ] 几乎，差不多
+deaf[def] 聋的
+frightened[ˈfraɪtnd ] 害怕的，受惊的
+roar [rɔ:(r)]吼叫声
+fox狐狸
+at night在夜晚
+Module 8
+coin[kɔɪn] 硬币
+ever[ˈevə(r) ] 曾经
+tidy整齐的，整洁的
+messy[ˈmesi ] 凌乱的
+never[ˈnevə(r)] 从不
+blackboard黑板 
+Module 9
+peace[pi:s]和平
+make peace维护和平
+member state成员国
+famous[ˈfeɪməs]著名的
+Module 10
+aunt姑母；姨母；舅母；伯母；婶母
+forgot（forget的过去式）忘记
+way方式，方法
+cross穿过（马路等）；渡过（河）
+六年级下
+Module 1 
+hot dog热狗
+cashier[kæˈʃɪə(r) ] 收银员
+cola可乐,一杯可乐
+soup[su:p] 汤
+dollar[ˈdɒlə(r) ] 美元
+cent[sent ] 美分
+enjoy[ɪnˈdʒɔɪ ] 享用，享受
+meal[mi:l ] 餐
+Module 2 
+later后来,以后
+to go剩余
+dark[dɑ:k ] 黑色的，黑暗的
+cloud云
+dry干的
+stay保持，维持
+cloudy多云的 
+Module 3 
+close合上，关闭
+quickly[ˈkwɪkli] 快速地
+left（leave的过去式）丢下
+cry喊，叫
+shine（太阳）发光，照耀 
+Module 4 
+thing东西,事物
+need需要
+grab[græb] 抓住；抓取
+balloon[bəˈlu:n] 气球
+fly away飞走
+stairs（常复）楼梯 
+Module 5 
+telephone电话
+ring响
+bell门铃
+bark（狗）吠,叫
+arrive [əˈraɪv ]到达
+hardly[ˈhɑ:dli] 几乎不
+raincoat [ˈreɪnkəʊt]雨衣
+tunnel [ˈtʌnl]隧道
+Module 6 
+home回家
+earth [ɜ:θ]地球
+space[speɪs ] 太空
+travel [ˈtrævl]旅行
+interested [ˈɪntrəstɪd] 感兴趣的
+be interested in对……感兴趣
+spaceship [ˈspeɪsʃɪp ]宇宙飞船
+surprised [səˈpraɪzd ]惊奇的；惊讶的
+made（make的过去式）做，制作
+model模型；模范，榜样
+taikonaut（中国的）太空人，宇航员
+national [ˈnæʃnəl ]国家的
+flag [flæg ]旗
+seed [si:d ]种子 
+Module 7 
+flew（fly的过去式）飞，飞行
+spent（spend的过去式）度过
+video [ˈvɪdiəʊ]录像
+someday [sʌmdeɪ ]有朝一日
+born [bɔ:n]出生，诞生
+illness [ˈɪlnəs ]病，疾病
+round在……各处
+all over the world全世界 
+Module 8 
+cup杯子
+classmate [ˈklɑ:smeɪt]同班同学
+baseball棒球
+brought（bring的过去式）带来
+more更强烈地
+mistake [mɪˈsteɪk] 错误
+make mistakes犯错误
+word单词，字
+
+
+
+
+
+
+
+
+Module 9
+wish愿望；希望，祝，祝愿
+best wishes最美好的祝福
+luck[lʌk] 运气
+primary [ˈpraɪməri ] 初级的，小学的
+primary school小学
+keep[ki:p] 保留
+forever[fərˈevə(r) ] 永远
+joy[dʒɔɪ ] 乐趣
+future[ˈfju:tʃə(r) ] 未来
+taught[tɔ:t ] （teach的过去式）教，讲授
+wonderful[ˈwʌndəfl ] 极好的，了不起的
+happiness[hæpɪnəs] 幸福，快乐 
+
+
+
+
+
+
+
+
+
+
+Module 10 
+middle中部的，中等的，中间
+middle school中学
+speech[spi:tʃ ] 演说，讲演
+September[sepˈtembə(r) ] 九月
+excited[ɪkˈsaɪtɪd ] 激动地，兴奋的
+geography[dʒiˈɒgrəfi ] 地理
+same相同的
+at the same time同时
+little[ˈlɪtl ] 极少量的
+keep on保持
+sometime某个时候
+each other互相；彼此
+chemistry [ˈkemɪstri ]化学
+physics[ˈfɪzɪks]物理
+`.split('\n');
+ const name = '外研版新标准小学英语';
+ const classify = '小学英语';
+ const fs = require('fs');
+
+
+ function Book(name){
+    return {
+        name,
+        classify,
+        children:[]
+    }
+ }
+ function Item(name,children){
+    let item = {
+        name
+    }
+    if(children){
+        item.children = children;
+    }
+    return item;
+}
+function saveBook(book){
+    let path = 'wordbooks/'+classify+'-'+book.name+'.json';
+
+    fs.createWriteStream(path).end(JSON.stringify(book,null,'\t'));
+}
+ let book;
+ let children;
+ source.forEach((l,i)=>{
+    if(/^[一二三四五六]年级/.test(l)){
+        let bookName=l.slice(0,4);
+        if(book){
+            saveBook(book);
+        }
+        book = Book(bookName);
+    }else if(/^(Module\s*?\d{1,2})/.test(l)){
+        let itemName=RegExp.$1;
+        children = [];
+        item = Item(itemName,children);
+        book.children.push(item);
+    }else if(/(^\w[\w\s]*?\w\b)/.test(l)){
+        let itemName=RegExp.$1;
+        children.push(Item(itemName));
+    }  
+    if(i===source.length-1){
+       saveBook(book);
+    }
+ });
