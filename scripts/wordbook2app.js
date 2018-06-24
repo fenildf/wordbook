99e0c13421754966d2771b22de4e1072b81abb62
Spencer <pallyoung@163.com>
2018-06-24 16:13:22 +0800

    wordbook2app

---
+++ scripts/wordbook2app.js
@@ -0,0 +1,69 @@
+//单词本修改转移到app目录下。
+
+const fs = require('fs');
+const PATH = 'wordbookapp/app/asserts';
+const ASSERT_PATH = `${PATH}/words.js`;
+const BOOK_DIR = 'wordbooks';
+
+if(!fs.existsSync(PATH)){
+    fs.mkdirSync(PATH);
+}
+
+const files = fs.readdirSync(BOOK_DIR);
+
+
+function iterator(){
+    let i = -1;
+    let books;
+    function next(){
+        i++;
+        if(!books){
+            books = getBooks();
+        }
+        let book = books[i]
+        if(book){
+            return book;
+        }else{
+            books = null;
+            return null;
+        }
+    }
+    return {next};
+};
+
+let books = [];
+
+files.forEach(path=>{
+    if (/\.json$/.test(path)) {
+        let data = JSON.parse(fs.readFileSync('wordbooks/' + path));
+        let bookname = data.name;
+        let classify = data.classify;
+        let content = `#${classify}#${bookname}`;
+
+        data.children.forEach(function (section) {
+            let sectionname = section.name;
+            let words = section.children.map(function (word) {
+                return word.name;
+            });
+            content+= `#${sectionname}:${JSON.stringify(words)}`
+        });
+        books.push(content);
+    }
+});
+
+let fileContent = `
+function getBooks(){
+    return ${JSON.stringify(books)}
+}
+${iterator.toString()}
+const version = ${Date.now()};
+export default {
+    iterator,
+    version
+}
+`;
+
+fs.createWriteStream(ASSERT_PATH).end(fileContent);
+
+
+
