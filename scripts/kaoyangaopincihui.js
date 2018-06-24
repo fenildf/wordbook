cf429d71dce7035e366c7b148773969e4874797b
Spencer <pallyoung@163.com>
2018-06-24 11:32:13 +0800

    考研高频词汇

---
+++ scripts/kaoyangaopincihui.js
@@ -0,0 +1,1082 @@
+const source = `历年考研英语高频词汇(单词精华版)
+abnormal不正常的
+absurd荒谬的
+abundant丰富的
+acute敏锐的，锋利的，急性的
+accurate 精确的，准确的
+adequate 足够的，恰当的，合适的
+adjacent（to）毗连的，接近的
+adverse不利的，相反的
+aerial空中的，航空的
+aesthetic审美的，有审美能力的
+agreeable宜人的，同意的
+aggressive侵略的好斗的，进取的
+alien外国的，不同性质的
+ambiguous模棱两可的模糊的 
+ambitious有雄心的有抱负的
+ample充足的，足够的，广大的
+analytical 分析的
+anonymous 匿名的 
+applicable 适用的，合适的
+apprehensive 不安的，担心的
+appropriate合适的，恰当的
+apt敏捷的；适当的；倾向，易于 
+arrogant 傲慢的，自大的
+authentic可靠的可信的
+authoritative 有权威的，可信的
+autonomous 自治的，自主的
+average 一般的普通的
+awesome 令人畏惧的
+aware 意识到的，知道的
+awkward 尴尬的
+bald秃头的，不毛的，单调的
+barren贫瘠的 不毛的
+bizarre 古怪的，奇异的
+bleak阴冷的，寒冷的；荒凉的；暗淡的
+blunt 不尖的，钝的；直率的，直言的
+botanical植物学的 
+bound 一定的
+brisk 活跃的，活泼的
+brutal 野蛮的，残忍的
+cardinal主要的，最基本的
+carefree  无忧无虑的
+casual 随意的，随便的
+catholic 广泛的，普遍的
+cautious 谨慎的，小心的
+chronic慢性的
+cognitive认知的
+coherent 一致的，连贯的；清晰，易懂的
+compact紧凑的 小巧的
+commercial 商业的，
+compassionate 表示同情的
+competent 有能力的
+competitive竞争性的，具有竞争力的
+compact紧凑的，小巧的，简洁的
+complementary 互补的
+compulsory强迫的，强制的，义务的
+concise 简洁的，简明的
+conclusive 确定的，决定性的
+confidential机紧的，秘密的
+conscientious负责的，尽责的
+conscious意识到的，知道的 
+consecutive 连续不断的，接连而来的
+consequent 随后发生的，由引起的
+conservative 保守的，传统的
+consistent 和…一致
+conspicuous 显而易见的，引人注目的
+constituent 组成的，构成的
+conventional 传统的
+courteous 有礼貌的
+cordial热情的，诚恳的
+corporate 法人的，公司的，全体的，
+cozy/cosy 舒适的
+crisp 脆的，有霜气的；干脆的
+crucial/critical  关键的
+cumulative 累积的
+current当前的
+decent 体面的，像样的，还不错的
+deceptive 欺骗的,骗人的
+definitive 最后的，明确的
+delicate 精细的，微妙的，精心处理的
+democratic 民主的
+desirable 理想的，想要的
+deteriorating 恶化的，变坏的
+destined 命中注定的
+destructive 毁灭的
+detached 超然的，
+detrimental 有害的
+digital 数字的
+divisive 分裂的，不和的
+disastrous 灾难性的
+discrete 不连续的，分立的
+divine 神圣的，神的
+dominant 占优势的，有支配力的
+dramatic 戏剧性的，夸张性的
+drastic 激烈的，猛烈的
+dreadful 可怕的
+dubious 怀疑的，可疑的
+eccentric 怪癖的，古怪的，离心的
+economic 经济的
+edible 可食的，无毒的
+effective 有效果的
+efficient 有效率的
+elegant优雅的，优美的，精致的
+eligible 合格的
+eloquent 有口才的，善辩的 
+embarrassing 令人尴尬的
+energetic 精力充沛的 
+equivalent 相等的
+erroneous 错误的 
+essential 必要的，基本的
+esthetic审美的
+eternal永恒的，无休止的
+evitable 可以避免的
+excessive 过分的，过度的
+exclusive独有的，排他的
+exotic 外来的，奇特的
+explicit 清楚的
+exquisite 优美的，精致的；灵敏的，敏锐的 
+extinct 灭绝的，绝种的
+extensive 广泛的
+extravagant 浪费的，奢侈的，过分，过度的
+fabulous 神话中有名的，难以相信的
+fanciful 奇怪的，不真实的
+fatal 致命的，毁灭性的
+feasible 可行的
+feeble 脆弱的，虚弱的
+feminine 妇女的，女性的，阴的
+fertile 肥沃的，有才智的，能生的
+financial 财政的，经济的
+finite 有限度的
+flexible 灵活的，能适应环境的
+formidable 可怕的，艰难的
+forthcoming 即将到来的；现成的；热心的 
+fragile 易碎的，易受伤害的
+fragrant 芳香的
+furious 猛烈的，生气的
+fruitful 有成效的
+generous 大方的
+genetic 基因的
+gigantic 巨大的
+global 全球的
+gloomy 暗淡的，忧伤的
+gorgeous 华丽的，灿烂的
+gracious 亲切的，大方的
+grim 严厉的，严格的，冷酷的 
+hasty 仓促的，匆忙的
+homogeneous 同类的，同性质的 
+hostile 敌对的
+humble 卑微的
+hysterical 歇斯底里的
+ideal 理想的
+identical相同的，一样的
+ignorant 无知的
+imaginable 可以想象的
+imaginative 富有想象力的，爱想象的  
+immune 有免疫力的，安全的
+impartial 公正的
+imperative 必要的，急切的，强制的
+incessant 不断的，不停的
+inaccessible不可接近的，不能达到的
+incredible 难以置信的 
+inadequate 不充分的，不适当的
+inclusive 包括的，包含的
+incurable 不治之症的
+indefinite 不确定的，模糊的
+indicative 直述的，指示的，表示的
+indifferent 不关心的，冷漠的
+indignant 生气的，愤怒的
+indispensable 必不可少的
+infectious传染的，传染性的
+innate/inborn 天生的
+inferior较次的，较劣的,地位较低的，较差的  
+inherent 固有的，生来的 
+influential 有影响力的
+ingenious 天才的，灵巧的，精美的
+innocent 无辜的，天真的
+inspirational 灵感的
+instantaneous 瞬间的，即时的
+instrumental 乐器的，有帮助的 
+intent 专心的，专注的
+intentional 故意的
+intelligible 易理解的，易领悟的
+interactive 相互的，互动的
+intermittent 间歇的，断续的
+internal 内部的
+intimate 亲密的
+intricate 复杂精细的
+intrinsic  固有的，本质的，内在的
+invalid 无效的，伤残的，残废的 
+invaluable 无价的
+ironical 讽刺的
+irreplaceable 不能替换的，不能代替的
+irrespective (of )不考虑，不顾  
+jealous 嫉妒的
+judicial 法官的，法庭的，审判的
+juvenile 少年的
+legislative 立法的，法律的
+legitimate 合法的，正规的，合理的
+lenient 宽大的，不严厉的
+liberal 自由的
+linear 线的，线状的 
+literal 文字的，字面的，逐字逐句的
+literate 识字的，文雅的 
+locomotive 移动的，引起运动的
+lofty 高的，高尚的，高傲的 
+logical 合乎逻辑的
+luminous 发光的，明亮的；清楚的，容易的
+masculine 男性的，阳性的
+massive 大规模的，大量的
+mechanical 机械的
+medieval 中古的，中世纪的
+mental 心理的，精神的，
+metallic 金属的
+militant  好战的，尚武的
+merciful 仁慈的，宽大的
+mobile 活动的，流动的，移动的
+monetary 货币的
+muscular 肌肉的
+mythical 神话般的
+naive 自然而天真的，质朴的
+nasty 肮脏的
+negligible可忽略的，微不足道的
+nominal 名义上的，名字的，无价值的
+notable 显著的，著名的
+notorious臭名昭著的，声名狼藉的 
+numerical 数字的，表示数量的
+obedient 服从的，顺从的
+obscure 模糊不清的
+obscene 淫秽的，肮脏的
+optimistic 乐观的
+optional 可供选择的
+oriental 东方的
+original 原先的，最早的
+orthodox 正统的，公认的
+overt 公然的，公开的 
+overwhelming 势如破竹的，压倒性的
+passionate 热情的，多情的 
+pathetic悲哀的，悲惨的
+patriotic 爱国的
+perpetual 永久的，持久的
+persistent 坚持不懈的
+potential 可能的，潜在的
+poisonous 有毒的
+predominant卓越的，流行的，显著的
+prevalent 普遍的，流行的  
+primitive 原始的，早期的
+productive 有生产力的，多产的
+proficient  熟练的，精通的
+profound 深刻的，深远的
+prominent 突出的，杰出的
+prompt 即刻的，迅速的  
+prone (to) 易于，倾向于
+proper 恰当的
+prosperous 富裕的，繁荣的
+prudent 谨慎的；小心的
+qualitative 与性质有关的，定性的
+queer 奇怪的，古怪的
+radiant 光辉灿烂的，容光焕发的
+radical 激进的，根本的
+random 随意的，无目的的
+rash 匆忙的，卤莽的，草率的 
+raw 自然状态的，未加工的
+redundant 过多的，多余的 
+relevant与…有关的
+reliable 可靠的
+resistant 有抵抗力的
+respectable 可尊敬的
+rewarding值得的
+rigorous 严格的，严厉的；严酷的
+righteous 正义的，义愤的
+rough 粗略的，不精确的
+savage 野蛮的；凶猛的，残酷的
+sensitive 敏感的
+sheer完全的，十足的
+shrewd精明的
+simultaneous 同时的
+skeptical 怀疑的
+slight 轻微的，小的
+sound 合理的，健康的
+stable/steady 稳定的，不断的
+stationary 固定的
+strenuous 吃力的，奋发的，奋斗的
+subordinate 次要的，从属的
+substantial 巨大的，
+subtle微妙的，精巧的，细微的 
+sufficient 足够的
+superficial 肤浅的  
+surgical 外科的
+supplementary 附加的，补充的
+susceptible 易动感情的;敏感的;易受…影响的
+suspicious 怀疑的
+sustainable 可持续的
+sympathetic 有同情心的
+tedious 冗长的，乏味的
+tentative 试探的
+thermal 热的，热量的
+transient 短暂的，片刻的
+trivial 琐碎的，不重要的
+turbulent 动荡的，混乱的
+typical 典型的，有代表性的
+ultimate 最后的，最终性的
+unanimous 全体一致的，无异义的
+underlying 潜在的，根本的 
+universal 普遍的，普通的
+verbal 言语的，口头的，逐字的 
+versatile 多才多艺的
+vicious 恶的，恶意的，错误的 
+visible 看的见的
+vivid 生动的，栩栩如生的
+vocational 职业的
+void =invalid 无效的
+vulnerable 易受伤的，容易受到…影响的
+watertight 防水的
+weary 疲倦的，劳累的
+weird=odd=strange 奇怪的
+worth 值得的
+wretched可怜的, 破旧的, 恶劣的, 沮丧的
+amply 充足的，大量的
+barely=hardly=scarcely 几乎不
+deliberately 故意地,深思熟虑地,审慎地
+explicitly 明确地
+exclusively 仅仅；唯一
+implicitly 不清楚地
+forcibly 强行地，有力地
+formerly 原先地，以前，从前
+incidentally 附带地，偶然地
+increasingly 日益，越来越多地 
+inevitably 必然地，不可避免地
+intentionally 有意地，故意地
+invariably 永不变的，恒久的
+optimistically. 乐观地
+outwardly 表面上，外表上地
+practically 实际上；几乎，差不多
+presumably 大概，可能，据推测
+quarterly 按季地
+shabbily 破旧地，褴褛地，卑鄙地
+shrewdly 精明地，明智地 
+shortly 不久，很快
+simultaneously 同时发生地
+somewhat颇为，稍稍，有几分
+spontaneously 自发地，自然产生地
+startlingly 惊人地
+triumphantly(欣喜)胜利地,成功地 
+unexpectedly 意外地
+virtually 事实上，实际地 
+abundance  丰富，充裕
+access (to) 入口，通路，接触
+accessory  附件，附属品
+accommodation 膳宿；和解，调解，适应
+acknowledgement 承认，感谢，致谢
+admiration 欣赏
+adolescence  青年期
+adolescent 青年，少年
+admission 许可，入门费，门票；承认，供认
+advocate提倡者，拥护者
+affection 亲爱，爱意
+aggregate 合计，总数
+allowance 津贴；考虑，体谅
+aisle 走廊，通道
+alloy 合金
+ambition 野心，雄心
+ammunition  军火，弹药
+analogy 相似，比喻，类比
+anticipation 预期，期望
+apparatus 仪器，装置；器官
+appendix 附加物，附录
+appraisal 估计，估价，评价
+appreciation 感谢，感激
+array 陈列，一系列
+arena 比武场，竞技场，舞台
+armo(u)r 盔甲，装甲
+array 阵式，服装；一大批，一系列
+artery 动脉；干道，主流
+artillery大炮，炮兵
+aspiration 愿望，希望；渴望
+assassination 暗杀，行刺
+assurance 保证，自信，把握
+automation 自动化
+aviation 航空，飞行
+axis 轴，轴心，政治联盟
+ballet/ ballot 芭蕾，芭蕾舞剧
+bankrupt 破产；完全缺乏
+batch 一批，一捆
+bearing 举止，态度；关系，方面
+belly 肚子，腹部
+bias 偏见，倾向，癖性
+bishop 主教
+blunder 错误，大错
+budget 预算
+booth 棚子，摊位，亭
+botany  植物学
+boycott 联合抵制
+brace  链，曲柄
+bracket 托架，类别；分类
+breach 违犯，破坏，缺口
+breakdown 坏掉，出毛病；病倒，衰退
+brink （危险等）边缘
+brochure 小册子
+bruise 挫伤；碰伤，淤伤
+bust 半身像，胸部
+buzz 嗡嗡声
+bypass 辅助道路
+cannon  大炮，火炮
+canvas 帆布，油画
+capsule  胶囊
+caption  标题，题目；说明文字
+captive 俘虏，被捕获的（动物）
+capability 能力，才能
+cash 现金  
+casualty 灾祸；伤亡
+catastrophe  大灾害，灾祸
+cathedral 总教堂，大教堂
+caution  小心；谨慎，警告
+census 人口统计，人口调查
+chorus  合唱团，异口同声
+chunk 一大块，一厚块
+circulation  (书报的)发行量，循环，流通 
+circus 竞技场，马戏团
+clan 宗族，大家族，氏族
+clarity  清楚，透明
+clearance 清除；间隙，空间；结关手续
+climax  高潮，顶点
+cluster 团，族，小群
+coalition  联合，联盟
+collaboration 合著，合作，勾结，通敌
+commitment 承诺，许诺
+commentary 实况报道，注释
+commonplace  习以为常的事情
+compartment 隔间，车室
+competence  称职，能力，权限
+complication 复杂，困难；并发症
+compensation 补偿，赔偿
+complexion  肤色，面色，面貌；形式
+compliment  恭维，表扬，问候，致意
+conception  构思，构想；怀孕
+configuration 形状，外貌，轮廓，排列
+consideration 考虑
+constitution 组成，构成；体格，体质；宪法
+consultant  顾问  
+contempt 轻视，蔑视，鄙视，不尊敬
+contention 争辩，争论，论点  
+controversy 争论，辩论
+conversion 转化，改变，转变  
+corpse 尸体
+counterpart 相对应的人或物；对方
+courtesy 礼貌，谦恭
+coverage 新闻报导
+coward  胆小鬼，懦夫
+crumble 碎屑，细屑  
+damage 损坏
+debate 争论，辩论
+decree  法令，判决
+default 不负责，不到场，不还债
+defiance 反抗，不尊重
+deficit 赤字
+denial 否认，否定，拒绝给与(正义，权利)
+descendant 后裔，后代
+descent 下来，下去；祖先，祖籍；突袭
+destiny 命运，天命 
+discrepancy 不同，不符合
+digest 摘要，简编
+dilemma 窘境，困境
+distinction 区分，辨别
+disposition 排列,性情,气质;倾向;支配权,处理权
+disturbance 搅乱，动乱；不安，骚动
+diversion 转向，改道；消遣，娱乐；分散
+doctrine 教条，主义，学说 
+domain 领土，领域；范围
+dome 圆屋顶  
+drainage 排水，疏水
+drawback 缺陷，障碍，不利
+drought 干旱，旱灾
+edible 可食之物 
+elite 杰出的人才或人物，精华
+emergency 紧急情况  
+empirical 经验的，全凭实验的
+encouragement 鼓励
+enclosure 包围，函中信件
+essence 本质
+estimate 估计
+esteem 尊敬，尊重 
+excerpt 摘录，引述
+expedition 远征，远征队
+expenditure 开支
+expertise 鉴定，评价；知识和技术
+exposition 解释，说明
+extinction .灭绝
+facet 平面，刻面，方面
+fashion 方式，样子  
+feast 盛宴；赏心悦目的东西
+feat 技艺；伟绩
+federation 联邦制，联合会，联盟
+ferry 渡船
+fitting 试衣，试穿；固定装置，家具，设备
+flattery 奉承，阿谀之词  
+fragrance 香味，香气
+flaw 裂纹，瑕疵
+fortune  财产，大笔的钱
+fraction 小部分，一点
+friction 摩擦，不和，冲突
+fuse 保险丝，导火线
+fusion 融合，联合
+galaxy 星系
+garment 衣服
+glacier 冰河
+glamour 魅力，魔力，诱惑力
+gossip 闲话，闲谈，随笔，漫谈 
+guarantee 保证，保证书，保修单
+guilt 犯罪，罪状
+handicap 障碍，阻碍
+harassment 烦恼，困苦，侵扰
+harmony 与…协调一致，和谐
+heritage 遗产，继承物 
+hierarchy 等级制度
+hike 徒步旅行
+hinge 重点，关键，主旨
+hospitality  友好款待，好客
+hostage 人质
+humidity 潮湿，湿度
+hurricane 飓风
+hypothesis 假设，假说  
+ideology 思想方式，意识形态
+idiot 白痴，傻瓜 
+illusion 错觉，假象
+impetus 动力，推动力，刺激 
+incentive 刺激，动机，鼓励，诱因
+inertia 迟钝，呆滞；惰性，惯性
+ingredient 成分
+inlet 入口
+insight 理解，洞察力
+inspection 检查，视察  
+instinct 本能，直觉
+integrity 正直，诚实
+intersection 交叉，交叉口
+intuition 直觉
+intuition 直觉
+inventory 详细目录 存货，盘存
+junction 连接，会合，交叉点
+junk 破烂物，垃圾
+lace 花边，饰带
+atitude 纬度,地区;自由,自由的范围
+layman 外行人，门外汉
+liability 责任，义务；易于，倾向于
+likelihood 可能性
+locality 位置，地方；辨认地方的能力
+longitude 经度
+lounge 休息室，休息处  
+lease 租约，契约
+legislation 立法，法律
+limitation 局限性，缺点
+loyalty 忠诚，忠心
+luxury 奢侈，豪华
+magistrate 地方官吏/法官
+magnitude 大小，光度
+mansion 大厦，官邸
+manifestation  表现(形式)
+massacre 大屠杀
+meadow 草地，牧场
+mechanism 机械装置，机构，结构方式  
+melody 音律;歌曲,曲调;主调,旋律
+menace  危险，威胁
+migrant 移居者，候鸟
+minority少数；未成年
+misfortune 不幸，灾难
+momentum 动量，动力，推动力
+monopoly 专利权，独占，垄断；专利品
+morality 道德，美德
+mortgage 抵押，抵押品
+notion 概念，观念，理解
+novelty 新鲜，奇特，新奇物
+oath 誓约，誓言
+obligation (法律上或道义上的)责任
+offset 印刷术
+offspring 后代，子孙，幼子
+optimum 最佳，最适宜，最有利
+orientation 定方向，定方位，朝向
+outfit 服装，装备，用具
+occasion 场合
+opponent  敌人，对手
+ornament 装饰，装饰品
+overpass 天桥，路桥，高架桥
+paradox 反论
+parasite 寄生虫，食客
+participation 参加，加入
+partition 分割，分配，瓜分 
+pastime 消遣，娱乐
+pasture  草原，牧场
+patent 专利权
+patrol 巡逻，巡查；巡逻者
+patron 赞助人，资助人
+pattern 模式
+penalty 制裁，惩罚
+pension 养老金
+personality 人格，人性
+plateau 高地，高原
+pledge 保证，誓言
+plight 困境，苦境
+position位置，职位，职务
+posture 姿势；状态，情况；心境，态度
+predecessor 前任，原有的事物
+premise 前提，假设
+premium 保险费；报酬，奖金
+prescription 处方
+preservation 保护，防护
+prestige 威信，威望
+pretext 借口，托辞
+prey 猎物
+priority 优先(权)
+proceeding 进行，举止，行为，处置
+prophet 先知，鼓吹者，提倡者，预言者
+proposition 主张；提议；问题，难题
+prospect  前景，可能性
+proximity 接近，接近于
+pursuit 追逐，追求；职业，工作
+questionnaire 调查表，问卷
+racket 喧嚣，闹声，繁忙
+rate 速度
+ration 比率
+recession  (经济)衰退，不景气；撤退
+reconciliation 和解，调停，顺从，听从
+reflection 反映，表现
+refreshment 爽快，舒心；提神的东西
+refuge 躲避，避难，避难所
+refund 退还，退款
+regime  政体，制度 
+regiment 团，多数，大量
+rehearsal 演习，排演，详述
+reign 主权，统治；朝代，王朝
+rein 缰绳
+rejoice 欢喜，快乐
+relay 替换，替班，接力，转播
+reliance 依靠，信任，信赖
+relish 吸引力，热心，兴趣
+remainder 剩余物或人
+remnant  残余，剩余
+repression 镇压，抑制，压抑
+reputation 名声，声望  
+resemblance 相似，类似
+reservation  贮存，贮藏
+reservation 预订，保留
+retention 保持，保留
+retrospect 回顾，回望
+revelation 显示，泄露
+revenue  税收，岁入
+rival 竞争对手
+routine 常规，惯例，例行公事
+rupture 破裂，绝交
+safeguard 安全设施或措施
+salvation 拯救，救助
+savage 野人，野蛮人
+scent 气味，香味；踪迹，痕迹
+scene 景色，景象
+scorn  轻蔑，鄙视
+scrutiny 细察，详审，核算选票
+sensation 感觉，激动，轰动
+sentiment 感情,情绪,情操;伤感;意见,观点
+setback 阻碍，挫折
+shortage 短缺，不足
+smash 打碎，粉碎
+stability 稳定(性)，稳固
+stack 堆，一堆
+standard 标准
+subsidy 补助金，资助金
+surface 表面
+suspension 暂停，延缓，停职，吊销
+synthesis 综合，合成，合成物
+temperament 气质，性格
+tempo 数率，进度，节奏
+testimony 证言，证词；宣言，陈述
+threshold 开端，入口
+tolerance  容忍，忍耐力
+transaction 处理，办理，交易
+transition 过渡，转变
+trend  倾向，趋势
+turnover 营业额，销售金额
+variation 变化，变动
+vegetation 植物，草木
+vein 心情，心思，意向
+verdict 裁决，判决；决定，判断
+versus 相对，对抗
+vicinity 接近，附近；密切的关系
+void 空虚，空处
+warehouse 货仓
+warfare 作战，交战
+warrant 根据，权威，委任状
+abandon  抛弃，放弃
+accommodate 供给住宿，答应; 适应
+acknowledge 承认，供认；致谢，打招呼
+acquaint 熟悉，认识，通晓
+acquire 获得，得到
+activate 使活波，激活
+addict (to) 沉溺于，嗜好
+adhere to 坚持，忠于
+adjoin 邻近，接近，毗连
+administer 管理，处理，治理，实行，实施
+adore 崇拜，敬爱，爱慕
+affiliate 加盟，入会
+affirm 肯定，断言
+afflict 受折磨，使苦恼
+afford 付得起，冒危险；供给，给予
+aggravate 使恶化，使严重
+alienate 离间，使不和，使疏远，转移，让渡
+allege 断言，宣称，提出
+alleviate 减轻，缓和
+allocate 分配，配给，拨给
+alternate 交替，轮流
+amend 改善，改良，改正；修正
+anticipate  预期，盼望
+appease 使平息，安抚
+applaud 赞扬，称赞
+articulate 清晰说话，连接
+ascend 上升，攀登
+ascertain 确知，探知
+ascribe  归因于，归功于
+assemble 集合，聚集，装备
+assign 分派，指派(职务，任务)
+assimilate 吸收，同化
+attribute...to 归因于
+authorize/ise 授权，委托；批准，许可  
+avail 利用，有帮助，有用
+avert 转移，防止，避免
+baffle 困惑，阻止，难住
+beam 微笑，高兴地笑；播送（节目等）
+base  建立在……的基础上
+bewilder 迷惑，弄糊涂
+blaze 燃烧；照耀
+blink 眨眼，闪烁不定
+blush 脸红，羞愧，羞于承认
+boost 提高，上扬，吹嘘
+breach 冲破，突破
+breed (over ) 培育，养育
+brew 酿制，调制；造成，形成，酝酿
+brood 沉思，思考，思虑
+browse 食，吃；浏览，随便阀阅
+bully 威胁，欺负
+carve 雕刻，铭刻，刻字
+cater (for) 准备，提供，迎合
+certify 证明，保证
+chant 唱歌，反复唱歌颂
+clamp 施压，钳制，用力制止
+clasp 紧握，紧抱
+cling (to )坚守，抱紧
+clutch 抓住，攫取，扑捉  
+coincide with 相同，相一致
+collaborate 合著，合作，合伙
+collide 互撞，碰撞
+commemorate 纪念，庆祝  
+commend 称赞，赞扬
+commence 开始
+commute 改变，换算成；减刑，通勤
+compensate  补偿，赔偿
+compile 编辑，编制，编写
+complement 补充，补足，结合  
+comply with  遵守
+conceive  想出，设想
+concern 涉及
+condense 压缩，浓缩
+conflict  冲突，战争
+conform 符合，遵守，适应
+confront  面对，面临
+conserve  保护，保存
+confer 授予；商谈，商讨，讨论
+console 给予安慰或同情，慰问
+consolidate 巩固，统一
+constrain 强迫，勉强，驱使
+contemplate 注视，沉思，打算
+contend 奋斗，竞争；主张，断言
+contrive 发明，设计，想办法，设法完成
+convene 召集，召开，集合
+converge 汇合，聚集
+convey 表达，传达
+correlate 有关系，发生联系
+corrode 腐蚀，侵蚀，受损害
+corrupt 腐败，败坏，行贿
+crash  (飞机)坠毁，冲入，破产，垮台
+cruise 航行，漫游，行进
+culminate 到达极点
+dart 飞奔，投射，冲进
+dazzle 使眩目，耀眼
+deceive 欺骗，哄骗
+decline 下降，减少，衰退
+dedicate 奉献，献身，致力于
+deduce 推理，推想，演绎出
+deem 相信，认为
+default 不负责，不到场，不还债
+defend 为……辩护
+defy 违抗，藐视
+degenerate 退步，堕落，腐化，恶化
+degrade 降级，堕落
+denote 代表，指出，指示
+denounce 揭发，告发
+deny 否认，剥夺，拒绝给
+deport 驱逐出境
+deprive 剥夺
+derive  得来，得到
+descend 下落
+descend 下来，下去；遗传，为后代
+deserve 值得
+designate 标明，指明，命名
+despise 鄙视，轻视，瞧不起
+detach 解开，拆开，使分离
+detain 扣留，使等候，阻止，耽搁
+deteriorate 恶化，变坏，变质
+deviate (使)背离，(使)偏离
+differentiate 区别，辨别，分别
+diffuse 散布，传播，扩散，推广
+dilute 变稀，变淡，稀释；减弱
+diminish 减少，缩小
+disable 使无能力，使残废
+discern 辩明，认清
+discriminate 区别，辨别；歧视
+dispense 分配，分给，施给；配药
+disperse 驱散，散开
+displace 移走，移置
+disguise 假扮，伪装
+disrupt 分裂，打破
+distort 歪曲，扭曲
+dominate 统治，占据
+dodge 闪避，躲闪，逃避，规避
+donate 捐赠，赠送
+doom 判定，注定
+dread 害怕，畏惧
+drain 渐渐耗尽
+duplicate  复制，重复
+eject 离开，逐出；喷出，射出  
+elapse 经过，过去，逝去
+elevate 举起，提高，使高尚
+elicit 发出，引出
+eliminate  消除，淘汰，剔除
+embark 乘船，装载；开始，从事
+embody 体现，包括，包含
+endow 捐赠，赋有
+endure 忍受，忍耐
+enhance 提高，增加
+enroll 使成为…的成员，注册
+ensue 随着发生，因而发生
+erase 擦掉，抹去，抹掉
+erupt 爆发，迸发
+escort 护送，护航
+evacuate 撤离，撤退，疏散
+evaporate 蒸发，消失，死亡
+exempt (from) 使免除
+evoke 引起，唤起
+exclaim 呼喊，欢呼
+expire 到期，期满
+explore 探险，探索
+extract 拔出，拔取；榨出；摘录
+fabricate 建造，装置；捏造，伪造
+flank 侧翼包围
+flap  (鸟)振翅(飞行)，挥动，轻拍
+flare 燃烧，闪烁，突然爆发
+flush 变红，脸红，激动
+formulate 明确的表达，阐述
+foster 照顾，培育，培养，养育
+follow 遵从，接受
+furnish . 配备，装饰
+gasp 喘气，喘息
+gauge 精确计量；估计，判断
+gaze  凝视，注视
+gear (to )使适应，使适合
+glitter 闪烁
+graze 吃草，放牧
+grease 涂上，搽上
+grieve  使伤心，使悲伤
+grin 露齿笑
+groan 呻吟
+grope 摸索，搜寻
+hamper 妨碍，限制
+haul 拖，拉
+haunt 常去，常出没，萦饶心头
+hinder 阻碍，妨害
+hoist  举起，升起，吊起
+hover 盘旋，翱翔
+huddle 挤在一起，缩成一团
+hurl 投掷，猛投
+identify 认出，确认
+ignite 引燃，发火
+illuminate 照亮，说明，阐明
+immerse (in )使浸没，使专心
+impair 损害，伤害
+impart 传授，给予；通知，告知  
+impose 把…强加于
+inaugurate 举行就职仪式，是…开始
+incorporate 结合，合并
+incur 招致，蒙受  
+induce 劝诱，诱导
+indulge 纵容，放任
+inflict on 强加于，使受痛苦
+inhabit 居住
+inhibit 阻止，禁止，抑制
+intend 意欲，打算
+interpret 解释，说明
+initiate 开始，着手，准许加入；传授
+inject 注射，注入
+insulate 绝缘，绝热，隔离
+intervene 干涉，阻扰，调停
+intrigue 设阴谋；激起好奇心
+invert 倒转，颠倒
+irritate 激怒，使急噪；使不舒服
+jeopardize 危及，损坏
+jerk 猛拉，颠簸而行
+jog 轻摇，缓缓行进
+lash 猛击，煽动
+levy 征收，征集；扣押
+linger 逗留，徘徊，留恋；迟缓，拖延
+litter 乱丢，乱堆
+locate 位于，定位
+loom 隐隐地出现，压在心头
+lubricate 加润滑油，使顺利进展
+lure 吸引，诱惑
+magnify 放大，垮大，夸奖
+manifest 显示，清楚表示，表明
+mean 打算，意欲
+mediate 想，考虑，沉思，思考
+merge 合并，逐渐融入
+mingle 混合起来，相混合
+minimize 对…做最低估计
+mob 包围（攻击）
+mobilize 动员
+mock 愚弄，嘲弄；使无法得逞
+monitor 监听，监控
+mourn 悲悼，哀悼，道歉
+necessitate 使必要
+neglect 忽视，遗漏
+nominate 提名，指定，指派
+nurture 养育，教养
+occupy 占领，使忙碌
+oppress 压迫，使难受
+orient 朝向东方，认清形式
+originate 首创，起源
+outrage 侵害，触犯，违反
+overflow 溢出，漫出，洋溢
+overhear 偷听，无意中听到
+overlap 重叠，相同，交叉
+overwhelm 压倒，浸没，使不安
+parade 游行
+paralyze/yse 使麻痹，使瘫痪
+pedal 踩踏板
+peel 剥，削
+perish 毁灭，死亡
+permeate 渗入，渗透
+perplex 使困惑，使迷惑
+plead 辩护，答辩；恳求，请求
+poke 推，刺，塞
+ponder 考虑，沉思
+preach 宣传，传教，鼓吹，倡导
+precede 在前，居先，先于
+preclude 阻止，使不可能
+prescribe 开处方，开药
+preside  主持，管理
+prolong 延长，拖延
+propel 推进，推动
+protest 抗议，反对
+provoke 引起，激起
+quantify 表示或测定。。。之量
+quench 熄灭，结束，灭绝
+radiate 辐射状发出，从中心向各方伸展出
+reassure 使安心，消除疑虑
+recede 后退，越远
+reconcile 使和好，调解
+rectify 改正，矫正
+refresh 提神，使清新
+refrain 抑制，克制
+refute 证明…不对(是错误的)，驳诉
+relish 享受，爱好，喜好
+remain 停留，依旧是
+repel 抗御，抵拒，使厌恶，使不愉快
+reproach 责备
+rescue 营救，救援
+reside (in) 居留，住，存在于
+resign 辞职
+resort (to )求助，凭借，诉诸
+resume 重新开始，继续
+retort 反驳，反击
+retrieve 获得，找回，补偿，解救，挽回
+revenge 报仇，报复
+revive 恢复，复活
+salute 行礼，致意
+scramble 爬，攀爬，爬行 ；争取，争夺
+scan 细察，审视，审视
+scrape 剥下，刮下
+scratch 抓，搔，勾掉，退出
+scrub 用力搽干，搽洗
+segregate 隔离，分开
+shatter 使粉碎，使破灭，
+shrink 收缩，减少
+shutter 关闭
+shuttle 往返移动
+standardize使标准化
+sieve 过滤
+signify 表示，意味，有关系
+simulate 假装，伪装，伪造，模仿
+smuggle 偷运，走私，偷带
+snatch 攫取，抢
+sneak 潜行，偷偷走
+sniff 嗤之以鼻，嗅
+sparkle 闪闪发光，闪烁
+steer 驾驶，引导
+strengthen 加强，使更强壮
+stretch  伸展，延伸
+strive奋力，抗争，搏斗
+stumble 绊倒，跌倒
+stun 击晕，使目瞪口呆
+subscribe 预订，订阅
+suck  (用嘴)吸，吞噬，卷入
+suffice 足够，使满足
+suppress 镇压，平定，制止
+surpass 超越，胜过
+sustain 承受，支撑，承住
+swamp 淹没，使透不过气来
+tackle 解决，处理
+tempt 引诱，劝诱
+terminate 终止，结束
+terrify使恐怖，惊吓
+testify 作证，证明，提供证据
+transmit 传播，传递，遗传，传导
+trigger 引发，引起
+tumble 跌倒，跌落；滚动，辗转
+unify 使合一，统一，结合
+update 使现代化，合时代
+uphold 坚持，支持，赞成
+ventilate 使通风，使空气流通，公开讨论
+verify 证实，证明
+visualize/ise 想象，设想
+view  视为，看做
+whirl 回旋，旋转；晕眩，纷乱
+withhold 抑制，制止，隐瞒，保留 
+wrench 猛拉，猛扭；歪曲，曲解
+wreck  (船只)失事，毁灭，破坏`.split('\n').map((l)=>{
+    if(/(^[\w\s\-]+)/.test(l)){
+        let word = RegExp.$1;
+        word = word.trim();
+        return word;
+    }else{
+        return '';
+    }
+});
+source.sort();
+const name = '考研高频词汇';
+const classify = '大学英语';
+const fs = require('fs');
+
+function Book(name){
+    return {
+        name,
+        classify,
+        children:[]
+    }
+ }
+ function Item(name,children){
+    let item = {
+        name
+    }
+    if(children){
+        item.children = children;
+    }
+    return item;
+}
+function saveBook(book){
+    let path = 'wordbooks/'+classify+'-'+book.name+'.json';
+
+    fs.createWriteStream(path).end(JSON.stringify(book,null,'\t'));
+}
+
+let book;
+let children;
+let pre='1';
+book = Book(name,[]);
+source.forEach((l,i)=>{
+    if(/^([\w\s\-]+)/.test(l)){
+       let itemName=RegExp.$1;
+       if(pre&&pre[0].toLowerCase()!=itemName[0].toLowerCase()){
+        children = [];
+        book.children.push(Item(itemName[0].toUpperCase(),children));
+       }
+       pre = itemName;
+       children.push(Item(itemName));
+   }  
+   if(i===source.length-1){
+      saveBook(book);
+   }
+});
