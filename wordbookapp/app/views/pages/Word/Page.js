6a9552b8af94ccaed6d9b05d427986ccc6cd672e
Spencer <pallyoung@163.com>
2018-06-30 21:55:24 +0800

    viewpaer

---
+++ wordbookapp/app/views/pages/Word/Page.js
@@ -0,0 +1,24 @@
+'use strict'
+import React,{Component} from 'react';
+
+import {
+    View
+} from 'react-native';
+
+class Page extends Component{
+    constructor(...props){
+        super(...props);
+        this.state={}
+    }
+    setData(){
+
+    }
+    render(){
+        return (
+            <View>
+            </View>
+        );
+    }
+}
+
+export default Page;
