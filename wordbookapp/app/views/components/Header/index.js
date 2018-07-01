a38547d56b19c7cb3db7bda798bfbf5d3ae8e7b2
Spencer <pallyoung@163.com>
2018-07-01 17:18:13 +0800

    header修改

--- wordbookapp/app/views/components/Header/index.js
+++ wordbookapp/app/views/components/Header/index.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import Text from './../Text';
 import TouchableOpacity from './../TouchableOpacity';
-import FontIcon from './../FontIcon';
 import StyleSheet from './../../../util/StyleSheet';
-import {dispatch} from 'febrest';
-import actions from './../../../constants/actions'
 class Button extends Component {
     constructor(...props) {
         super(...props);
@@ -33,73 +29,29 @@ class Title extends Component {
 export default class Header extends Component {
     constructor(...props) {
         super(...props);
-        this.state = {
-            title: this.props.title,
-            canGoBack: false,
-            header: this.props.header === null ? false : true,
-            leftButton: this.props.leftButton,
-            rightButton: this.props.rightButton,
-            onLeftButtonPress:this.props.onLeftButtonPress,
-            onRightButtonPress:this.props.onRightButtonPress,
-            props: this.props
-        }
     }
-    static getDerivedStateFromProps(nextProps, prevState) {
-        let props = prevState.props;
-        let nextState = {}
-        if (nextProps.title !== props.title) {
-            nextState.title = nextProps.title;
-        }
-        if (nextProps.leftButton !== props.leftButton) {
-            nextState.leftButton = nextProps.leftButton;
-        }
-        if (nextProps.rightButton !== props.rightButton) {
-            nextState.rightButton = nextProps.rightButton;
-        }
-        nextState.props = nextProps;
-        return nextState;
-    }
-    _mergePropsToState(){
-
-    }
-    updateInfo(info) {
-        this.setState(info);
-    }
+   
     _renderLeftButton() {
-        let {routeState} = this.props;
         if (typeof this.state.leftButton === 'object') {
             return <Button
                 type='leftButton'
-                onPress={()=>this.state.onLeftButtonPress&&this.state.onLeftButtonPress()}
-                children={this.state.leftButton} />;
-        } else if (routeState.index !== 0) {
-            return <Button
-                type='leftButton'
-                onPress={()=>dispatch(actions.APP_NAVIGATE_GOBACK)}
-                children={this._backButton()} />;
+                onPress={()=>this.props.onLeftButtonPress&&this.props.onLeftButtonPress()}
+                children={this.props.leftButton} />;
         }
         return <View style={styles.emptyLeftButton}/>
-    }
-    _backButton() {
-        return <FontIcon style={styles.backArrow} name='ios-arrow-round-back-outline'/>
     }
     _renderRightButton() {
         if (typeof this.state.rightButton === 'object') {
             return <Button
                 type='rightButton'
-                onPress={()=>this.state.onRightButtonPress&&this.state.onRightButtonPress()}
-                children={this.state.rightButton} />;
+                onPress={()=>this.props.onRightButtonPress&&this.props.onRightButtonPress()}
+                children={this.props.rightButton} />;
         } else {
             return null;
         }
     }
     _renderTitle() {
         var child;
-        if (typeof this.state.title === 'function') {
-            child = this.state.title;
-        } else {
-            child = <Text style={[styles.titleText]}>{this.state.title}</Text>
-        }
         return <Title>{child}</Title>
     }
     render() {
@@ -131,13 +83,6 @@ const styles = StyleSheet.create(function(theme){
             overflow: 'hidden',
             flexDirection: 'row',
             alignItems: 'center',
-        },
-        titleText:{ 
-            fontSize: theme.navigationHeaderFontSize,
-            color:theme.navigationHeaderColor
-        },
-        backArrow:{
-            color:theme.navigationHeaderColor
         },
         button:{
             flexDirection: 'row',
